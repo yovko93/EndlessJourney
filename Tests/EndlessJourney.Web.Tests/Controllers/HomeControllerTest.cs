25e6cd9e9e44470268b1eb8bf8d18dce4b283ed0
Yovko Garkov <iovko1993@gmail.com>
2022-04-20 23:25:35 +0300

    Add home controller tests

---
+++ Tests/EndlessJourney.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿namespace EndlessJourney.Web.Tests.Controllers
+{
+    using EndlessJourney.Web.Controllers;
+    using EndlessJourney.Web.ViewModels.Home;
+    using FluentAssertions;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using static EndlessJourney.Web.Tests.Trips;
+
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexShouldReturnCorrectViewWithModel()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithData(ThreeTrips))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<HomeViewModel>()
+                    .Passing(model => model.Should().NotBeNull()));
+
+        [Fact]
+        public void ErrorShouldReturnView()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Error())
+                .ShouldReturn()
+                .View();
+    }
+}
---
+++ Tests/EndlessJourney.Web.Tests/Data/Trips.cs
@@ -0,0 +1,15 @@
+﻿namespace EndlessJourney.Web.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using EndlessJourney.Data.Models;
+
+    public static class Trips
+    {
+        public static IEnumerable<Trip> ThreeTrips
+            => Enumerable.Range(0, 10).Select(t => new Trip
+            {
+            });
+    }
+}
