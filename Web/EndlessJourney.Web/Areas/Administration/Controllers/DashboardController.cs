e2edf953e1af1f6a0858fc4fe383f883f61241a4
Yovko Garkov <iovko1993@gmail.com>
2022-04-23 20:19:16 +0300

    Add canvas js chart

--- Web/EndlessJourney.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/EndlessJourney.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,16 +1,27 @@
 ﻿namespace EndlessJourney.Web.Areas.Administration.Controllers
 {
-    using EndlessJourney.Services.Data;
-    using EndlessJourney.Web.ViewModels.Administration.Dashboard;
+    using System.Collections.Generic;
 
+    using EndlessJourney.Web.ViewModels.Administration.Dashboard;
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
 
     public class DashboardController : AdministrationController
     {
         public IActionResult Index()
         {
-            var viewModel = new IndexViewModel { SettingsCount = 5, };
-            return this.View(viewModel);
+            List<DataPoint> dataPoints = new List<DataPoint>();
+
+            dataPoints.Add(new DataPoint("Samsung", 25));
+            dataPoints.Add(new DataPoint("Micromax", 13));
+            dataPoints.Add(new DataPoint("Lenovo", 8));
+            dataPoints.Add(new DataPoint("Intex", 7));
+            dataPoints.Add(new DataPoint("Reliance", 6.8));
+            dataPoints.Add(new DataPoint("Others", 40.2));
+
+            this.ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return this.View();
         }
     }
 }
